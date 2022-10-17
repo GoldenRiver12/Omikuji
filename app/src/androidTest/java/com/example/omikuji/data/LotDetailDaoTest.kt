566fc3803ee2c7be67df613f162c7934a3d3cd13
GoldenRiver12 <gandhi9736@gmail.com>
2022-10-17 20:39:10 +0900

    [add] テストを追加

---
+++ app/src/androidTest/java/com/example/omikuji/data/LotDetailDaoTest.kt
@@ -0,0 +1,51 @@
+package com.example.omikuji.data
+
+import android.content.Context
+import androidx.room.Room
+import androidx.test.core.app.ApplicationProvider
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import kotlinx.coroutines.runBlocking
+import org.junit.After
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.assertj.core.api.Assertions.*
+
+@RunWith(AndroidJUnit4::class)
+class LotDetailDaoTest {
+
+    private lateinit var lotDetailDao: LotDetailDao
+    private lateinit var db: AppDatabase
+
+    @Before
+    fun createDb() = runBlocking{
+        val context = ApplicationProvider.getApplicationContext<Context>()
+        db = Room.inMemoryDatabaseBuilder(context, AppDatabase::class.java).build()
+        lotDetailDao = db.lotDetailDao()
+        lotDetailDao.insertAll(testLotDetails)
+    }
+
+    @After
+    @Throws(Exception::class)
+    fun closeDb() {
+        db.close()
+    }
+
+    @Test
+    @Throws(Exception::class)
+    fun testGetLotDetails(){
+        val lotDetails = lotDetailDao.getLotDetails()
+        assertThat(lotDetails)
+            .hasSize(3)
+            .hasSameElementsAs(testLotDetails)
+    }
+
+    companion object{
+        val testLotDetails = listOf(
+            LotDetail("大吉"),
+            LotDetail("小吉"),
+            LotDetail("凶")
+        )
+    }
+
+}
